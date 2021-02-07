63de3a1ace11905f3ee66564660c83826a691de9
nedakinduryte <neda.kinduryte@gmail.com>
2021-02-07 14:02:41 +0000

    extracting only relevant data

--- scraper.js
+++ scraper.js
@@ -1,8 +1,12 @@
-const axios = require("axios");
+const axios = require('axios');
+const cheerio = require('cheerio');
 
-const url = "https://www.amazon.co.uk/deal/7006cf95";
+const url = 'https://www.amazon.co.uk/deal/41b5f944';
 
 axios(url).then(response => {
-    const html = response.data;
-    console.log(html)
+    const data = cheerio.load(response.data);
+    const extracted = cheerio.html(data('div #octopus-dlp-asin-stream'));
+    console.log(extracted);
 }).catch(console.error);
+
+
